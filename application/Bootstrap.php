664a6b7cd6a61265772d6a42f5a88e50a035afcd
firstboy0513 <guohua0594@sina.com>
2013-10-09 23:55:43 +0800

    Add Zend Framework

---
+++ application/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+	protected function _initAuthvalid() {
+		$fc = Zend_Controller_Front::getInstance ();
+		require(APPLICATION_PATH . '/modules/auth/plugins/Valid.php');
+		$fc->registerPlugin ( new Auth_Plugin_Valid() );
+	}
+}
+
