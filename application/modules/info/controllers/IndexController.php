9f15b5442280ffe0a64d7eb27ac4bda0ed776040
firstboy0513 <guohua0594@sina.com>
2013-10-29 22:37:06 +0800

    Add info module

---
+++ application/modules/info/controllers/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+class Info_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+    	$this->forward('aboutus');
+    }
+    
+    public function aboutusAction()
+    {
+    	$this->view->info = true;
+    }
+    
+    public function joinusAction()
+    {
+    	$this->view->info = true;
+    }
+    
+    public function contactusAction()
+    {
+    	$this->view->info = true;
+    }
+
+}
+
