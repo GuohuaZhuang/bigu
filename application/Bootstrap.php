5c87b78e13240f1affc4e03f85411acbebd7f3ef
firstboy0513 <guohua0594@sina.com>
2013-12-26 11:50:28 +0800

    remove php powered and apache version, server operation system, and change php default session name PHPSESSID

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,9 +7,10 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		require(APPLICATION_PATH . '/modules/auth/plugins/Valid.php');
 		$fc->registerPlugin ( new Auth_Plugin_Valid() );
 	}
-	protected function __initSession() {
+	protected function _initSession() {
 		ini_set('session.gc_maxlifetime', 1209600);  // set session max lifetime seconds
-		ini_set('session.gc_divisor', 1);       // 100% chance of running GC
+		ini_set('session.gc_divisor', 1);            // 100% chance of running GC
+		ini_set('session.name', 'BIGUSID');          // bigu session id
 		Zend_Session::start();
 		// So, I can have in my applicatin.ini
 		// phpSettings.session.gc_maxlifetime = 1209600
